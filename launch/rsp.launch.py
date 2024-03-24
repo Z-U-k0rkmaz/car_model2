c9444f5beb13002c55cdf1d18665ae8b77ee3bcb
zeynel ümit korkmaz <101003417+Z-U-k0rkmaz@users.noreply.github.com>
2024-03-24 17:25:41 +0300

    Add files via upload

---
+++ launch/rsp.launch.py
@@ -0,0 +1,50 @@
+# bu launch dosyasının amacı robot state publisher ı başlatmak
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration, Command
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+
+import xacro
+
+
+def generate_launch_description():
+    
+    # simülasyon zamanı için başlatma argümanlarını tanımla
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    use_ros2_control = LaunchConfiguration('use_ros2_control')
+
+    # urdf dosyasını işleme
+    pkg_path = os.path.join(get_package_share_directory('car_model2'))
+    xacro_file = os.path.join(pkg_path,'description','car.urdf.xacro')
+    #robot_description_config = xacro.process_file(xacro_file)
+    robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time])
+    
+    # robot state publisher başlatma
+    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher', 
+        executable='robot_state_publisher', 
+        output='screen', 
+        parameters=[params]
+    )
+    
+    
+    # launch tanımlama
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time', 
+            default_value='false', 
+            description='Use sim time if true'
+        ),
+        DeclareLaunchArgument(
+            'use_ros2_control', 
+            default_value='true', 
+            description='Use ros2_control if true'
+        ),
+        
+        node_robot_state_publisher
+    ])
